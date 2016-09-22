0cbc70b5e9b0bb71153b6814ddba234300f64abd
andygout <andygout@hotmail.co.uk>
2016-09-22 21:55:05 +0100

    Amend: Delete button to send delete value as hidden input element

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,9 +3,17 @@ const router = express.Router();
 const path = require('path');
 const pg = require('pg');
 const connectionString = require(path.join(__dirname, '../', '../', 'config'));
+const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 
-router.use(methodOverride('_method'));
+router.use(bodyParser.urlencoded({ extended: true }))
+router.use(methodOverride(function(req, res){
+	if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+		const method = req.body._method;
+		delete req.body._method;
+		return method;
+	}
+}))
 
 const handleConnectionErrors = (err, done, res) => {
 	// Handle connection errors
