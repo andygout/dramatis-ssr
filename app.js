e98f73cf4e651632511f81216e36e7bf7afe1d5d
andygout <andygout@hotmail.co.uk>
2016-10-02 21:24:22 +0100

    Amend: Comments in app.js

--- app.js
+++ app.js
@@ -33,17 +33,15 @@ app.use(
 
 app.use('/', routes);
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function (req, res, next) {
 	const err = new Error('Not Found');
 	err.status = 404;
 	next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
+// Error handlers
+// Development error handler - will print stacktrace
 if (app.get('env') === 'development') {
 	app.use(function (err, req, res, next) {
 		res.status(err.status || 500);
@@ -54,8 +52,7 @@ if (app.get('env') === 'development') {
 	});
 }
 
-// production error handler
-// no stacktraces leaked to user
+// Production error handler - no stacktraces leaked to user
 app.use(function (err, req, res, next) {
 	res.status(err.status || 500);
 	res.render('error', {
