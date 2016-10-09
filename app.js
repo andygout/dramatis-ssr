39c650eaebced870db7f5526c6dbb184f865806d
Andy Gout <andygout@users.noreply.github.com>
2016-10-09 19:11:53 +0100

    Refactor: App files (#17)
    * Extract: www file into app file
    
    * Remove: Unused node packages (until later required)
    
    * Amend: Setup plugin syntax from double quotes to single
    
    * Correct: Usage of path.join function

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
 import express from 'express';
 import exphbs from 'express-handlebars';
 import favicon from 'serve-favicon';
+import http from 'http';
 import logger from 'morgan';
 import path from 'path';
 import sassMiddleware from 'node-sass-middleware';
@@ -17,19 +17,18 @@ app.set('view engine', 'html');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(favicon(path.join(__dirname, './client/favicons', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'client', 'favicons', 'favicon.ico')));
 app.use(logger('dev'));
 
 app.use(
 	sassMiddleware({
-		src: __dirname + '/client/stylesheets',
-		dest: __dirname + '/client/public',
+		src: path.join(__dirname, 'client', 'stylesheets'),
+		dest: path.join(__dirname, 'client', 'public'),
 		prefix:  '/stylesheets',
 		debug: true,
 	})
 );
-app.use(express.static(path.join(__dirname, './client', 'public')));
+app.use(express.static(path.join(__dirname, 'client', 'public')));
 
 app.use('/', router);
 
@@ -61,4 +60,41 @@ app.use(function (err, req, res, next) {
 	});
 });
 
-module.exports = app;
+const normalizePort = val => {
+	const port = parseInt(val, 10);
+	if (isNaN(port)) return val;
+	if (port >= 0) return port;
+	return false;
+}
+
+const onError = error => {
+	if (error.syscall !== 'listen') throw error;
+	const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port
+
+	switch (error.code) {
+		case 'EACCES':
+			console.error(bind + ' requires elevated privileges');
+			process.exit(1);
+			break;
+		case 'EADDRINUSE':
+			console.error(bind + ' is already in use');
+			process.exit(1);
+			break;
+		default:
+			throw error;
+	}
+}
+
+const onListening = () => {
+	const addr = server.address();
+	const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+	console.log('Listening on ' + bind);
+}
+
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
