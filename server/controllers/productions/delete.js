56e2bb44e398c2f5e899c2cdc6c652c194cf51af
andygout <andygout@hotmail.co.uk>
2016-10-30 11:54:24 +0000

    Use: Hidden id value to identify production to be deleted

--- server/controllers/productions/delete.js
+++ server/controllers/productions/delete.js
@@ -2,7 +2,7 @@ import Production from '../../models/production';
 import { setAlert } from '../../../lib/alert';
 
 export default function (req, res, next) {
-	const production = new Production(req.params);
+	const production = new Production(req.body);
 
 	production.delete(function (err, data) {
 		if (err) return next(err);
