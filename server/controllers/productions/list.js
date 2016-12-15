ee7df209adbd377d919d6578a8d8b7c3ab124551
Andy Gout <andygout@users.noreply.github.com>
2016-12-15 19:58:52 +0000

    Refactor codebase (#37)
    * Remove: Explicit current directory path for npm script commands
    
    * Use: Tilde (not caret) for node packages at major version 0
    
    * Amend: Use of Object.assign() assigning to new object when not required
    
    * Remove: handleModelResponse import in production list controller
    
    * Amend: Fixtures so format is consistent
    
    * Remove: Spaces between square brackets and content when array is single value
    
    * Rename: File to transfer development environment variables to .env
    
    * Remove: File suffix from required files
    
    * Add: field__input class line-height property w/value 1.2

--- server/controllers/productions/list.js
+++ server/controllers/productions/list.js
@@ -1,9 +1,8 @@
 const Production = require('../../models/production');
-const handleModelResponse = require('../../lib/handle-model-response');
 const getAlert = require('../../lib/alert').get;
 
 module.exports = function (req, res, next) {
 	return Production.list()
-		.then(data => res.render('list', Object.assign({}, data, { alert: getAlert(req) })))
+		.then(data => res.render('list', Object.assign(data, { alert: getAlert(req) })))
 		.catch(err => next(err));
 };
