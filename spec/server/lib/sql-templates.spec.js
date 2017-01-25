4651930cf94b64a2fda58c385b67e9f7bf706fe7
andygout <andygout@hotmail.co.uk>
2017-01-25 20:52:40 +0000

    Amend: SQL Templates module spec to not reset model stubs unnecessarily

--- spec/server/lib/sql-templates.spec.js
+++ spec/server/lib/sql-templates.spec.js
@@ -9,14 +9,12 @@ const stubs = {
 	format: {
 		literal: sinon.stub().returns('\'pgFormatValue\'')
 	},
-	Production: null,
-	Theatre: null
+	Production: sinon.createStubInstance(Production),
+	Theatre: sinon.createStubInstance(Theatre)
 };
 
 const resetStubs = () => {
 	stubs.format.literal.reset();
-	stubs.Production = sinon.createStubInstance(Production);
-	stubs.Theatre = sinon.createStubInstance(Theatre);
 };
 
 beforeEach(function () {
