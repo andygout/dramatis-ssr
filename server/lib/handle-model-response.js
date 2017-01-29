903fff18b5258cf12b7b9d9c399f845830792a78
andygout <andygout@hotmail.co.uk>
2017-01-29 18:19:10 +0000

    Remove: Unnecessary template literals from Handle Model Response module

--- server/lib/handle-model-response.js
+++ server/lib/handle-model-response.js
@@ -9,8 +9,8 @@ module.exports = function (req, res, data) {
 	data[page.modelName].hasError ?
 		(page.action === 'create' || page.action === 'update') ?
 			res.render(`${page.modelRoute}/form`, Object.assign(data, { alert: alert.get(req) })) :
-			res.redirect(`${page.instanceRoute}`)
+			res.redirect(page.instanceRoute)
 		:
-		res.redirect(page.action !== 'delete' ? `${page.instanceRoute}` : '/');
+		res.redirect(page.action !== 'delete' ? page.instanceRoute : '/');
 
 };
