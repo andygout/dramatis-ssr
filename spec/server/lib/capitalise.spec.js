48f30ec04a093092f6f7a694a9153e21d5be1e99
andygout <andygout@hotmail.co.uk>
2017-03-03 18:45:15 +0000

    Rename: Capitalise module test description (now no longer exclusively handlebars)

--- spec/server/lib/capitalise.spec.js
+++ spec/server/lib/capitalise.spec.js
@@ -2,7 +2,7 @@ const expect = require('chai').expect;
 
 const subject = require('../../../dist/lib/capitalise');
 
-describe('Capitalise handlebars helper', () => {
+describe('Capitalise module', () => {
 
 	it('will return string with initial letter as capital', () => {
 		expect(subject('string')).to.eq('String');
