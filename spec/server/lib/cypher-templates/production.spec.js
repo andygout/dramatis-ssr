4273c047fc0746a53a76c83426898dede6448e4c
andygout <andygout@hotmail.co.uk>
2017-07-04 22:04:56 +0100

    Delete: Cypher templates tests for functions that simply return query (non-dynamically)

--- spec/server/lib/cypher-templates/production.spec.js
+++ spec/server/lib/cypher-templates/production.spec.js
@@ -44,35 +44,6 @@ describe('Cypher Templates Production module', () => {
 
 	});
 
-	describe('getEditQuery function', () => {
-
-		it('will return requisite query', () => {
-
-			const result = subject.getEditQuery();
-			expect(removeWhitespace(result)).to.eq(removeWhitespace(`
-				MATCH (production:Production { uuid: $uuid })-[:PLAYS_AT]->(theatre:Theatre)
-				OPTIONAL MATCH (production)-[:PRODUCTION_OF]->(playtext:Playtext)
-				OPTIONAL MATCH (production)<-[castRel:PERFORMS_IN]-(person:Person)
-				OPTIONAL MATCH (person)-[roleRel:PERFORMS_AS { prodUuid: $uuid }]->(role:Role)
-				WITH production, theatre, playtext, castRel, person, roleRel, role
-				ORDER BY roleRel.position
-				WITH production, theatre, playtext, castRel, person,
-					CASE WHEN role IS NULL THEN [] ELSE COLLECT({ name: role.name }) END AS roles
-				ORDER BY castRel.position
-				RETURN {
-					model: 'production',
-					uuid: production.uuid,
-					name: production.name,
-					theatre: { name: theatre.name },
-					playtext: CASE WHEN playtext IS NULL THEN '' ELSE { name: playtext.name } END,
-					cast: CASE WHEN person IS NULL THEN [] ELSE COLLECT({ name: person.name, roles: roles }) END
-				} AS production
-			`));
-
-		});
-
-	});
-
 	describe('getUpdateQuery function', () => {
 
 		it('will return requisite query', () => {
@@ -108,39 +79,6 @@ describe('Cypher Templates Production module', () => {
 					model: 'production',
 					uuid: production.uuid,
 					name: production.name
-				} AS production
-			`));
-
-		});
-
-	});
-
-	describe('getShowQuery function', () => {
-
-		it('will return requisite query', () => {
-
-			const result = subject.getShowQuery();
-			expect(removeWhitespace(result)).to.eq(removeWhitespace(`
-				MATCH (production:Production { uuid: $uuid })-[:PLAYS_AT]->(theatre:Theatre)
-				OPTIONAL MATCH (production)-[playtextRel:PRODUCTION_OF]->(playtext:Playtext)
-				OPTIONAL MATCH (production)<-[castRel:PERFORMS_IN]-(person:Person)
-				OPTIONAL MATCH (person)-[roleRel:PERFORMS_AS { prodUuid: $uuid }]->(role:Role)
-				OPTIONAL MATCH (role)<-[roleRel]-(person)-[castRel]->(production)-[playtextRel]->
-					(playtext)-[:INCLUDES_CHARACTER]->(character:Character) WHERE role.name = character.name
-				WITH production, theatre, playtext, castRel, person, roleRel, role, character
-				ORDER BY roleRel.position
-				WITH production, theatre, playtext, castRel, person, CASE WHEN role IS NULL THEN [{ name: 'Performer' }] ELSE
-					COLLECT({ model: 'character', uuid: character.uuid, name: role.name }) END AS roles
-				ORDER BY castRel.position
-				RETURN {
-					model: 'production',
-					uuid: production.uuid,
-					name: production.name,
-					theatre: { model: 'theatre', uuid: theatre.uuid, name: theatre.name },
-					playtext: CASE WHEN playtext IS NULL THEN null ELSE
-						{ model: 'playtext', uuid: playtext.uuid, name: playtext.name } END,
-					cast: CASE WHEN person IS NULL THEN [] ELSE
-						COLLECT({ model: 'person', uuid: person.uuid, name: person.name, roles: roles }) END
 				} AS production
 			`));
 
