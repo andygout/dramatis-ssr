efbc6a53a018b1efaf135ab42929720b13e612db
Andy Gout <andygout@users.noreply.github.com>
2020-06-07 15:52:52 +0100

    Update strings module (#105)
    * Add pluralise + singularise functions to strings module
    
    * Enable capitalise function to handle uppercase input
    
    * Revise pluralise + singularise tests context wording

--- src/lib/strings.js
+++ src/lib/strings.js
@@ -1,5 +1,16 @@
-const capitalise = (string) => string.charAt(0).toUpperCase() + string.substring(1);
+import {
+	IRREGULAR_PLURAL_NOUNS_MAP,
+	IRREGULAR_SINGULAR_NOUNS_MAP
+} from '../utils/constants';
+
+const capitalise = (string) => string.charAt(0).toUpperCase() + string.substring(1).toLowerCase();
+
+const pluralise = model => IRREGULAR_PLURAL_NOUNS_MAP[model] || `${model}s`;
+
+const singularise = pluralisedModel => IRREGULAR_SINGULAR_NOUNS_MAP[pluralisedModel] || pluralisedModel.slice(0, -1);
 
 export {
-	capitalise
+	capitalise,
+	pluralise,
+	singularise
 };
--- test/lib/strings.test.js
+++ test/lib/strings.test.js
@@ -1,14 +1,76 @@
 import { expect } from 'chai';
 
-import { capitalise } from '../../src/lib/strings';
+import { capitalise, pluralise, singularise } from '../../src/lib/strings';
 
 describe('Strings module', () => {
 
 	describe('Capitalise function', () => {
 
-		it('returns string with initial letter as capital', () => {
+		context('Input string is lowercase', () => {
 
-			expect(capitalise('string')).to.eq('String');
+			it('returns string with initial letter as capital', () => {
+
+				expect(capitalise('string')).to.eq('String');
+
+			});
+
+		});
+
+		context('Input string is uppercase', () => {
+
+			it('returns string with initial letter as capital', () => {
+
+				expect(capitalise('STRING')).to.eq('String');
+
+			});
+
+		});
+
+	});
+
+	describe('Pluralise function', () => {
+
+		context('Model has regular plural noun', () => {
+
+			it('returns singular noun with appended \'s\'', () => {
+
+				expect(pluralise('production')).to.eq('productions');
+
+			});
+
+		});
+
+		context('Model has irregular plural noun', () => {
+
+			it('returns specific plural noun', () => {
+
+				expect(pluralise('person')).to.eq('people');
+
+			});
+
+		});
+
+	});
+
+	describe('Singularise function', () => {
+
+		context('Model has regular plural noun', () => {
+
+			it('returns plural noun with tailing \'s\' removed', () => {
+
+				expect(singularise('productions')).to.eq('production');
+
+			});
+
+		});
+
+		context('Model has irregular plural noun', () => {
+
+			it('returns specific singular noun', () => {
+
+				expect(singularise('people')).to.eq('person');
+
+			});
 
 		});
 
