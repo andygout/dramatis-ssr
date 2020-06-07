a4d8909eee3e098c298500ca61adb9f8431e23c4
andygout <andygout@hotmail.co.uk>
2020-06-07 11:55:54 +0100

    Amend casing of constants

--- src/components/InstanceLink.jsx
+++ src/components/InstanceLink.jsx
@@ -1,12 +1,12 @@
 import { h } from 'preact';
 
-import { irregularPluralNounsMap } from '../utils/constants';
+import { IRREGULAR_PLURAL_NOUNS_MAP } from '../utils/constants';
 
 export default function (props) {
 
 	const { instance: { model, uuid, name } } = props;
 
-	const pluralisedModel = irregularPluralNounsMap[model] || `${model}s`;
+	const pluralisedModel = IRREGULAR_PLURAL_NOUNS_MAP[model] || `${model}s`;
 
 	const instancePath = `/${pluralisedModel}/${uuid}`;
 
