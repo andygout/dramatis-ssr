d5b32f4ff6dbb3f161c7a48b93e45aefa8326074
Andy Gout <andygout@users.noreply.github.com>
2021-08-31 22:17:07 +0100

    Fix react/display-name linting rule (#176)

--- src/controllers/helpers/render-component-to-string.jsx
+++ src/controllers/helpers/render-component-to-string.jsx
@@ -1,8 +1,10 @@
 import { h } from 'preact'; // eslint-disable-line no-unused-vars
 import render from 'preact-render-to-string';
 
-export default (PageComponent, props) => { // eslint-disable-line react/display-name
+const renderComponentToString = (PageComponent, props) => {
 
 	return render(<PageComponent { ...props } />);
 
 };
+
+export default renderComponentToString;
