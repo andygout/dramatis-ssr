92d561e5d422642b40f6ac789290a03f568ba621
Andy Gout <andygout@hotmail.co.uk>
2022-08-20 12:18:34 +0100

    Rename main.scss -> index.scss

--- webpack.config.js
+++ webpack.config.js
@@ -64,7 +64,7 @@ const serverConfig = {
 
 const clientConfig = {
 	mode: 'none', // i.e. not production or development (see: https://webpack.js.org/configuration/mode).
-	entry: './src/client/stylesheets/main.scss',
+	entry: './src/client/stylesheets/index.scss',
 	output: {
 		path: path.join(__dirname, 'public'),
 		filename: 'main.js'
