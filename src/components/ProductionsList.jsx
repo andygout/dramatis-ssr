be6647a8b99886a3eb696369c04d084f7c768378
Andy Gout <andygout@hotmail.co.uk>
2023-03-04 17:01:43 +0000

    Display production sub-sub-productions

--- src/components/ProductionsList.jsx
+++ src/components/ProductionsList.jsx
@@ -4,16 +4,22 @@ import { ListWrapper, ProductionLinkWithContext } from '.';
 
 const ProductionsList = props => {
 
-	const { productions } = props;
+	const { productions, isNested } = props;
 
 	return (
-		<ListWrapper>
+		<ListWrapper isNested={isNested}>
 
 			{
 				productions.map((production, index) =>
 					<li key={index}>
 
 						<ProductionLinkWithContext production={production} />
+
+						{
+							production.subProductions?.length > 0 && (
+								<ProductionsList productions={production.subProductions} isNested={true} />
+							)
+						}
 
 					</li>
 				)
