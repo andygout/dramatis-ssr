6195c2ae19e5b4ae776a0d956a07c8ea2ea55a0f
Andy Gout <andygout@hotmail.co.uk>
2023-08-20 21:29:01 +0100

    Display production name

--- src/pages/instances/Season.jsx
+++ src/pages/instances/Season.jsx
@@ -1,15 +1,27 @@
 import { h } from 'preact'; // eslint-disable-line no-unused-vars
 
-import { App } from '../../components';
+import { App, InstanceFacet, ProductionsList } from '../../components';
 
 const Season = props => {
 
 	const { currentPath, documentTitle, pageTitle, season } = props;
 
-	const { model } = season;
+	const { model, productions } = season;
 
 	return (
-		<App currentPath={currentPath} documentTitle={documentTitle} pageTitle={pageTitle} model={model} />
+		<App currentPath={currentPath} documentTitle={documentTitle} pageTitle={pageTitle} model={model}>
+
+			{
+				productions?.length > 0 && (
+					<InstanceFacet labelText='Productions'>
+
+						<ProductionsList productions={productions} />
+
+					</InstanceFacet>
+				)
+			}
+
+		</App>
 	);
 
 };
