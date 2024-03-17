668266e9a5e460f82e63dd6dc63c1fe6c106253c
Andy Gout <andygout@hotmail.co.uk>
2024-03-17 21:09:49 +0000

    Add search bar

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,10 +2,12 @@ import errors from './errors';
 import home from './home';
 import instances from './instances';
 import lists from './lists';
+import search from './search';
 
 export {
 	errors,
 	home,
 	instances,
-	lists
+	lists,
+	search
 };
