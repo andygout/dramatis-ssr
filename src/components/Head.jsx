2e9300a970f38e9d3fc516f7797c67f852d6157a
Andy Gout <andygout@users.noreply.github.com>
2025-12-21 10:41:22 +0000

    Convert native SCSS to CSS (#288)

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -5,8 +5,26 @@ const Head = props => {
 	return (
 		<head>
 			<title>{`${documentTitle} | Dramatis`}</title>
-			<link rel="stylesheet" href="/main.css" />
-			<script src="/main.js" />
+
+			{/* Ordering of stylesheet groups is important. */}
+			<link rel="stylesheet" href="/stylesheets/normalize.css" />
+
+			<link rel="stylesheet" href="/stylesheets/color-variables.css" />
+
+			<link rel="stylesheet" href="/stylesheets/footer.css" />
+			<link rel="stylesheet" href="/stylesheets/header.css" />
+			<link rel="stylesheet" href="/stylesheets/instance.css" />
+			<link rel="stylesheet" href="/stylesheets/list.css" />
+			<link rel="stylesheet" href="/stylesheets/navigation.css" />
+			<link rel="stylesheet" href="/stylesheets/page.css" />
+			<link rel="stylesheet" href="/stylesheets/text.css" />
+
+			<link rel="stylesheet" href="/stylesheets/origami.css" />
+
+			<link rel="stylesheet" href="/stylesheets/o-autocomplete-modifiers.css" />
+			<link rel="stylesheet" href="/stylesheets/o-forms-modifiers.css" />
+
+			<script src="/scripts/main.js" />
 		</head>
 	);
 
