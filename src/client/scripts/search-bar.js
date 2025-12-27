fcd18a86f71ad500802394304fe3e7572584d8cc
Andy Gout <andygout@hotmail.co.uk>
2025-12-27 16:22:49 +0000

    Trim searchTerm query param value

--- src/client/scripts/search-bar.js
+++ src/client/scripts/search-bar.js
@@ -19,7 +19,7 @@ async function performFetch (url) {
 
 async function getSearchResults (searchTerm) {
 
-	const url = `${URL_BASE}/api/search?searchTerm=${encodeURIComponent(searchTerm)}`;
+	const url = `${URL_BASE}/api/search?searchTerm=${encodeURIComponent(searchTerm.trim())}`;
 
 	const searchResults = await performFetch(url);
 
