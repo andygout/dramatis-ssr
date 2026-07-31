d26efcc5926d4d00b00a8190a9def6ff2ecbdf46
Andy Gout <andygout@users.noreply.github.com>
2026-07-31 17:42:33 +0100

    Display material settings (#343)

--- src/pages/instances/Locale.jsx
+++ src/pages/instances/Locale.jsx
@@ -1,11 +1,19 @@
-import { App } from '../../components/index.js';
+import { App, InstanceFacet, MaterialsList } from '../../components/index.js';
 
 const Locale = (props) => {
 	const { currentPath, documentTitle, pageTitle, locale } = props;
 
-	const { model } = locale;
+	const { model, materials } = locale;
 
-	return <App currentPath={currentPath} documentTitle={documentTitle} pageTitle={pageTitle} model={model} />;
+	return (
+		<App currentPath={currentPath} documentTitle={documentTitle} pageTitle={pageTitle} model={model}>
+			{materials?.length > 0 && (
+				<InstanceFacet labelText="Materials as setting">
+					<MaterialsList materials={materials} />
+				</InstanceFacet>
+			)}
+		</App>
+	);
 };
 
 export default Locale;
--- src/pages/instances/Time.jsx
+++ src/pages/instances/Time.jsx
@@ -1,11 +1,19 @@
-import { App } from '../../components/index.js';
+import { App, InstanceFacet, MaterialsList } from '../../components/index.js';
 
 const Time = (props) => {
 	const { currentPath, documentTitle, pageTitle, time } = props;
 
-	const { model } = time;
+	const { model, materials } = time;
 
-	return <App currentPath={currentPath} documentTitle={documentTitle} pageTitle={pageTitle} model={model} />;
+	return (
+		<App currentPath={currentPath} documentTitle={documentTitle} pageTitle={pageTitle} model={model}>
+			{materials?.length > 0 && (
+				<InstanceFacet labelText="Materials as setting">
+					<MaterialsList materials={materials} />
+				</InstanceFacet>
+			)}
+		</App>
+	);
 };
 
 export default Time;
