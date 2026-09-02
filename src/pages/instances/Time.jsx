3d4b267aeae129da95f792326dc8e2398d566da9
Andy Gout <andygout@hotmail.co.uk>
2026-09-02 19:50:33 +0100

    Display time surTimes + subTimes

--- src/pages/instances/Time.jsx
+++ src/pages/instances/Time.jsx
@@ -1,12 +1,24 @@
-import { App, InstanceFacet, MaterialsList } from '../../components/index.js';
+import { App, InstanceFacet, InstanceLinksList, MaterialsList } from '../../components/index.js';
 
 const Time = (props) => {
 	const { currentPath, documentTitle, pageTitle, time } = props;
 
-	const { model, materials } = time;
+	const { model, surTimes, subTimes, materials } = time;
 
 	return (
 		<App currentPath={currentPath} documentTitle={documentTitle} pageTitle={pageTitle} model={model}>
+			{surTimes?.length > 0 && (
+				<InstanceFacet labelText="Is within">
+					<InstanceLinksList instances={surTimes} />
+				</InstanceFacet>
+			)}
+
+			{subTimes?.length > 0 && (
+				<InstanceFacet labelText="Contains">
+					<InstanceLinksList instances={subTimes} />
+				</InstanceFacet>
+			)}
+
 			{materials?.length > 0 && (
 				<InstanceFacet labelText="Materials as setting">
 					<MaterialsList materials={materials} />
